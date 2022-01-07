288bbd7d98f3c38789f9330e1c3ade5b39b72d09
Aarón Aira <krustof.sea@gmail.com>
2022-01-07 14:59:12 +0100

    Avatar adds to map

--- main.js
+++ main.js
@@ -4,15 +4,16 @@ const fetchData = async () => {
     return data;
 }
 
-const user = 
 
 window.onload = () =>(
     fetchData().then(data => {
         const {comments, currentUser} = data
         comments.map((data, i)=> {
             document.querySelector(`#u${i} .card-user`).innerHTML = data.user["username"];
+            document.querySelector(`#u${i} figure img`).src = data.user["image"]["webp"];
             document.querySelector(`#u${i} .card-date`).innerHTML = data.createdAt;
-
+            document.querySelector(`#u${i} .card-body p`).innerHTML = data.content;
+            document.querySelector(`#u${i} .card-score span`).innerHTML = data.score;
         })
     })
 )
