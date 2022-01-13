fa646566b8b1beaf2f9f51fcaf71a76be1691cd3
Aarón Aira <krustof.sea@gmail.com>
2022-01-13 14:34:15 +0100

    arreglado localStorage, agregando replies

--- main.js
+++ main.js
@@ -1,40 +1,3 @@
-// const fetchData = async () => {
-//     const response = await fetch("data.json")
-//     const data = await response.json()
-//     return data;
-// }
-
-const updateStorage =  (data) => {
-  window.localStorage.setItem('comments', JSON.stringify(data))
-
-}
-
-const updateScore = (id, vote)=> { 
-  let score;
-
-  vote == 'plus' ? score = getComments["score"]++ : 
-  score = getComments["score"]--
-  
-  updateScore(score)
-
-}
-
-const getComments = ()=> {
-  fetch("data.json").then(res => res.json())
-  .then(data => window.localStorage.setItem('comments', JSON.stringify(data["comments"])))
-  
-  // actualizando dato
-
-  let data = JSON.parse(window.localStorage.getItem('comments'))
-  return data;
-  // // window.localStorage.getItem('data') ? console.log("data loaded") :
-  // // window.localStorage.setItem('data', JSON.stringify(data))
-
-  // const data = JSON.parse(window.localStorage.getItem('data'))
-  
-}
-
-
 const card = `
 <div id="$uid" class="card">
 <div class="card-header d-flex align-items-center justify-content-between">
@@ -79,60 +42,136 @@ const reply_card = `
   </div>
 </div>`
 
+const storage = window.localStorage
 
-const getLikes = ()=> {
-  let cardSelection = document.querySelectorAll("div.card")
-    cardSelection.forEach(el => {
-      el.addEventListener("click", (e)=>{
-        
-          console.log(e.target.className, e.currentTarget.id)
 
-        //   let userID = e.currentTarget.id[1]
-        //   getData()["comments"][userID]['score']++
-        //   window.localStorage.setItem('data', JSON.stringify(getData()))
-        //   e.target.nextElementSibling.innerHTML = getData()["comments"][userID]['score'];
-          
-        // }
-        // else if(e.target.classList.contains("minus")){
-        //   let userID = e.currentTarget.id[1]
-        //   let data = getData()["comments"][userID]['score']--
-        //   console.log(data)
-        //   e.target.previousElementSibling.innerHTML = getData()["comments"][userID]['score'];
-        
-      })
-    });
+
+
+const fetchData = async () => {
+  const response = await fetch("data.json")
+  const data = await response.json();
+  return [data][0];
 }
 
-/*
-* Map all comments and extract info about them
-*/
 
-getComments().map((data, i)=> {
 
-  let card_content = card.replace("$uid", "u"+i).replace("$uname",data.user["username"])
-  .replace("$uimg",data.user["image"]["webp"]).replace("$udate", data.createdAt)
-  .replace("$ucontent",data.content).replace("$uscore",data.score)
+const getData = async (type_data) => {
+  let data;
 
-  document.querySelector("div.container-fluid").innerHTML += card_content;
-  
-  // if comment has replies, then map the replies tho
- 
-  if(data.replies.length > 0) {
+  if(storage.getItem(type_data)) {
+    data =  JSON.parse(storage.getItem(type_data))
     
-    data.replies.map((reply, ir)=> {
-      document.querySelector("#u"+i).innerHTML += reply_card
+  } else {
+      console.log("no data in localstorage, fetching new data")
+      data = await fetchData()
+      data = data[type_data]
+      storage.setItem(type_data,JSON.stringify(data))
+    }
 
-    })
+  return data;
+  
+}
+
+const renderComments = async  () => {
+    
+    let data_ = await getData("comments")  
+
+    await data_.map((data, i)=> {
+    
+    let card_content =  card.replace("$uid", "u"+i).replace("$uname",data.user["username"])
+    .replace("$uimg",data.user["image"]["webp"]).replace("$udate", data.createdAt)
+    .replace("$ucontent",data.content).replace("$uscore",data.score)
+
+    document.querySelector("div.container-fluid").innerHTML += card_content;
+    
+    // if comment has replies, then map the replies tho
+    console.log(data.replies)
+  
+    if(data.replies.length > 0) {
+      
+      data.replies.map((reply, ir)=> {
+
+        let reply_card_content = reply_card.replace("$uid", "r"+reply.id).replace("$uname",reply.user["username"])
+        .replace("$uimg",reply.user["image"]["webp"]).replace("$udate", reply.createdAt)
+        .replace("$ucontent",reply.content).replace("$uscore",reply.score)
+        document.querySelector("#u"+i).innerHTML += reply_card_content
+
+      })
+    }
+  })
+}
+
+const updateStorage = (data) => {
+  storage.setItem('comments', JSON.stringify(data))
+
+}
+
+const loadScore = async (id) => {
+
+  let getScore = await getData("comments")
+  getScore = getScore[id]["score"]
+  return getScore;
+}
+
+const updateScore = async (id, vote, type)=> { 
+  
+  let comments = await getData("comments")
+  let updateScoreComment = comments
+
+  let updateScoreReply = comments
+  
+  if (type) {
+
+    vote == "plus" ? updateScoreComment[id]["score"]++ :
+    updateScoreComment[id]["score"]--
+    updateStorage(updateScoreComment)
+
+  } else {
+    
+    vote == "plus" ? updateScoreReply[id]["replies"][id]["score"]++ :
+    updateScoreReply[id]["score"]--
+    updateStorage(updateScoreReply)
+
   }
-})
+ 
+}
 
-getComments();
-getLikes();
+const getLikes =   () => {
+  let cardSelection =  document.querySelectorAll("div.card")
+    cardSelection.forEach(el => {
+      el.addEventListener("click", async (e)=>{
+        
+        await updateScore(e.currentTarget.id[1], e.target.className)
+        console.log(e.target)
+        e.currentTarget.querySelector(".card-score span").innerHTML = await loadScore(e.currentTarget.id[1]);
+    });
+  })
+}
+const getReplyLikes = () => {
+  let cardReplySelection = document.querySelectorAll("div.reply")
+  cardReplySelection.forEach(el => {
+    el.addEventListener("click", (e) => {
+      console.log(e.currentTarget)
+    })
+  })
+}
+/*
+* Render items
+*/
+const render = async () => {
+  await renderComments();
+  getLikes();
+  getReplyLikes();
+}
+
+
+render();
+
 
 // fetchData().then(data => {
-//   window.localStorage.setItem('data', JSON.stringify(data))
+//   storage.setItem('data', JSON.stringify(data))
 // })
-// console.log(Window.localStorage.getItem("data"))
+// console.log(storage.getItem("data"))
 
 // fetchData().then(data => {
 //     const {comments, currentUser} = data
